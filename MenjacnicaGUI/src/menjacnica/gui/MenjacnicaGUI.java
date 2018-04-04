0f3618c2c262ffa9c8708587695a586ef15f069e
lukadlm97 <Luka@Luka-PC>
2018-04-04 21:55:30 +0200

    Kreirana JFrame forma, kao i paketi

---
+++ MenjacnicaGUI/src/menjacnica/gui/MenjacnicaGUI.java
@@ -0,0 +1,71 @@
+package menjacnica.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.Dialog.ModalExclusionType;
+import javax.swing.JFormattedTextField;
+
+public class MenjacnicaGUI extends JFrame {
+
+	private JPanel contentPane;
+	private JButton btnNewButton;
+	private JFormattedTextField formattedTextField;
+	private JButton btnNewButton_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenjacnicaGUI frame = new MenjacnicaGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MenjacnicaGUI() {
+		setIgnoreRepaint(true);
+		setEnabled(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		contentPane.add(getBtnNewButton(), BorderLayout.WEST);
+		contentPane.add(getFormattedTextField(), BorderLayout.CENTER);
+		contentPane.add(getBtnNewButton_1(), BorderLayout.EAST);
+	}
+
+	private JButton getBtnNewButton() {
+		if (btnNewButton == null) {
+			btnNewButton = new JButton("New button");
+		}
+		return btnNewButton;
+	}
+	private JFormattedTextField getFormattedTextField() {
+		if (formattedTextField == null) {
+			formattedTextField = new JFormattedTextField();
+		}
+		return formattedTextField;
+	}
+	private JButton getBtnNewButton_1() {
+		if (btnNewButton_1 == null) {
+			btnNewButton_1 = new JButton("New button");
+		}
+		return btnNewButton_1;
+	}
+}
